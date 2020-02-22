190f2351915f2f0d31edd6cc3e225d3254822beb
diegosanchezstrange <diegosanchezstrangejavax@gmail.com>
2020-02-22 20:11:14 +0100

    Server now checks if the given JWT is valid

--- src/server/login/views.py
+++ src/server/login/views.py
@@ -69,6 +69,7 @@ class LoginUserApi(MethodView):
 
             if hased_passwd == stored_passwd[:64]:
                 token = jwt.encode({'user': name, 'exp': datetime.datetime.utcnow() + datetime.timedelta(hours=1)}, current_app.config['SECRET_KEY'])
+                print(token)
                 return jsonify({'token': token.decode('UTF-8')})
             else:
                 return jsonify('Could not verify!', 401, {'WWW-Authenticate': 'Basic realm="Login Required"'})
@@ -77,11 +78,33 @@ class LoginUserApi(MethodView):
 
     def get(self):
         return make_response(jsonify({'response':'Todo correcto!'})) 
-        
+
+class CheckJWT(MethodView):
+
+    def post(self):
+        data = request.get_json()
+        if data is None:
+            return make_response(jsonify({'message':'No JSON content found on the request'}))
+        if 'token' in data:
+            recived_token = data['token']
+
+            try:
+                data = jwt.decode(recived_token, current_app.config['SECRET_KEY'])
+            except jwt.ExpiredSignatureError:
+                return make_response(jsonify({'message':'Token expired'}))
+            except :
+                return make_response(jsonify({'message':'Token invalid'}))
+
+            return make_response(jsonify({'message':'Va bien la cosa'}))
+
+        return make_response(jsonify({'message':'No field named token found'}))
+
 
 register_user_api = RegisterUserAPI.as_view('register')
 login_user_api  = LoginUserApi.as_view('login')
+check_jwt_api = CheckJWT.as_view('check')
 test_api = TestAPI.as_view('test')
 bp.add_url_rule('/login', view_func=login_user_api, methods=['POST', 'GET'])
 bp.add_url_rule('/register', view_func=register_user_api, methods=['POST', 'GET'])
+bp.add_url_rule('/check', view_func=check_jwt_api, methods=['POST'])
 bp.add_url_rule('/', view_func=test_api, methods=['POST', 'GET'])
